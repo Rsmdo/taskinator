d415fca984ddf11e0d0f28864e5b74d7ca4ea039
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-03-08 15:11:52 -0500

    added abilityto add a task

---
+++ assets/js/java.js
@@ -0,0 +1,21 @@
+// var buttonEl =document.querySelector("#save-task");
+// console.log(buttonEl);
+// buttonEl.addEventListener("click",function(){
+//     document.createElement("li");
+// });
+
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+var createTaskHandler = function(){
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "this is a new task";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+
+buttonEl.addEventListener("click", createTaskHandler);
+
